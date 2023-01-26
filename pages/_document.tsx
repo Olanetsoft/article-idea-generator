c93f4e627d49e2429b826f5562b79c5b9010a262
Idris Olubisi <heedris2olubisi@gmail.com>
2023-01-26 14:53:36 +0100

    Configured Meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,16 +17,40 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="icon" href="/favicon.ico" />
+        {/* <!-- HTML Meta Tags --> */}
         <meta
           name="description"
           content="Generate your next article idea with ease."
         />
-        <meta property="og:site_name" content="articleideagenerator.com" />
+
+        {/* <!-- Google / Search Engine Tags --> */}
+        <meta itemProp="name" content="Article Idea Generator" />
+        <meta
+          itemProp="description"
+          content="Generate your next article idea with ease."
+        />
+        <meta
+          itemProp="image"
+          content="https://article-idea-generator.vercel.app/articleideagenerator.com/og-image.png"
+        />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta
+          property="og:url"
+          content="https://article-idea-generator.vercel.app"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Article Idea Generator" />
         <meta
           property="og:description"
           content="Generate your next article idea with ease."
         />
-        <meta property="og:title" content="Article Idea Generator" />
+        <meta
+          property="og:image"
+          content="https://article-idea-generator.vercel.app/articleideagenerator.com/og-image.png"
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Article Idea Generator" />
         <meta
@@ -34,13 +58,11 @@ export default function Document() {
           content="Generate your next article idea with ease."
         />
         <meta
-          property="og:image"
-          content="articleideagenerator.com/og-image.png"
-        />
-        <meta
           name="twitter:image"
-          content="articleideagenerator.com/og-image.png"
+          content="https://article-idea-generator.vercel.app/articleideagenerator.com/og-image.png"
         />
+
+        {/* <!-- Meta Tags Generated via http://heymeta.com --> */}
       </Head>
       <body>
         <Main />
