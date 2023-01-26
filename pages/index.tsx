c963ae26abdc7d5b2204c021b3ad4f4877f59223
Idris Olubisi <heedris2olubisi@gmail.com>
2023-01-26 13:48:20 +0100

    CLEAN UP

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,6 @@ export default function Home() {
     setGeneratedTitle("");
     setLoading(true);
 
-    console.log("prompt:", prompt);
 
     const response = await fetch("/api/generate", {
       method: "POST",
@@ -36,7 +35,6 @@ export default function Home() {
     });
 
     console.log("responding...");
-    console.log("response: ", response);
 
     if (!response.ok) {
       setLoading(false);
