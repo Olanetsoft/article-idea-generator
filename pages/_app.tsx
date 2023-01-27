5b5d4274de5001f1152f70f1d209e823f41a3315
Idris Olubisi <heedris2olubisi@gmail.com>
2023-01-27 22:43:43 +0100

    Feat: Dark mode and Light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <Analytics />
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+        <Analytics />
+      </ThemeProvider>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", 
   theme: {
     extend: {
       fontFamily: {
