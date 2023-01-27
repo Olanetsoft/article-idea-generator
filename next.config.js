8022029f2d317c495dc98244543668ba350c5d6c
Idris Olubisi <heedris2olubisi@gmail.com>
2023-01-27 15:02:41 +0100

    Fix: SIte Optimization and PWA

--- next.config.js
+++ next.config.js
@@ -8,3 +8,12 @@ module.exports = {
     domains: ["i.imgur.com", "www.google.com", "media.giphy.com"],
   },
 };
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  skipWaiting: true,
+});
