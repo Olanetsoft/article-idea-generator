868e2df18ef1599db9b2817a0e5c86f2df266816
Idris Olubisi <heedris2olubisi@gmail.com>
2023-03-27 00:24:37 +0100

    Fix: Optimized OpenAI stream payload for better responses

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -21,10 +21,8 @@ const handler = async (req: NextRequest): Promise<Response> => {
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
-    temperature: 0.7,
+    temperature: 1,
     top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
     max_tokens: 200,
     stream: true,
     n: 1,
