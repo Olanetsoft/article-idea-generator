cf8fbbc84615c54662839d0be0a24a5a11e46bbf
Idris Olubisi <heedris2olubisi@gmail.com>
2023-06-25 17:20:40 +0100

    chore: add api for mobile app

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -22,15 +22,21 @@ interface requestPayload {
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const prompt = req.body.prompt;
+    const seoEnabled = req.query.seoEnabled;
 
     // Validate the prompt
     if (!prompt) {
       return res.status(400).json({ error: "No prompt in the request" });
     }
 
+    const newPrompt =
+      seoEnabled && seoEnabled === "true"
+        ? `Generate 4 article title for ${prompt}. Ensure its SEO friendly titles with clickbait. Make sure its not more than 4, its relevant and not out out of context.`
+        : `Generate 4 article title for "${prompt}". Make sure its not more than 4, its relevant and not out out of context.`;
+
     const payload: requestPayload = {
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
+      messages: [{ role: "user", content: newPrompt }],
       temperature: 0,
       max_tokens: 600,
     };
@@ -48,7 +54,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const data = await response.json();
 
-    return res.json(data);
+    return res.json({
+      result: data.choices[0].message.content.replace(/"/g, ""),
+    });
   } catch (error) {
     console.log(error);
   }
