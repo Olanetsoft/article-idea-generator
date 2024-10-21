b505887d7ae300377abfa2f028e1fbae166f45e2
timmy471 <ayantunjitimilehin@gmail.com>
2024-10-21 11:52:53 +0100

    fix: checkbox state SEO Enabled

--- pages/index.tsx
+++ pages/index.tsx
@@ -153,16 +153,23 @@ export default function Home(): JSX.Element {
             <div className="bg-transparent border-2 rounded-md border-indigo-400 w-4 h-4 flex justify-center items-center mr-2 cursor-pointer">
               {seoEnabled && (
                 <svg
-                  className="fill-current w-3 h-3 text-indigo-600 pointer-events-none"
-                  viewBox="0 0 17 12"
                   xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="3"
+                  stroke="#4f46e5"
+                  className="w-4 h-4"
                 >
-                  <path d="m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z" />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m4.5 12.75 6 6 9-13.5"
+                  />
                 </svg>
               )}
             </div>
             <span className="select-none cursor-pointer">
-              Enable SEO & Clickbait Feature
+              Enable SEO &amp; Clickbait Feature
             </span>
           </label>
           {loading && (
